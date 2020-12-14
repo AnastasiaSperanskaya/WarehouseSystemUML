52b93c9336a888ecdaa7b5df88fff2924e182c75
AnastasiaSperanskaya <cookie003@mail.ru>
2020-12-14 22:36:55 +0500

    database class improved

--- DBManager.java
+++ DBManager.java
@@ -2,9 +2,7 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
-import com.mysql.jdbc.*;
 
 public class DBManager {
     private static final String URL = "jdbc:mysql://localhost:3306/warehouse?useSSL=false";
@@ -15,15 +13,10 @@ public class DBManager {
         Connection connection;
         Driver driver = new FabricMySQLDriver();
         DriverManager.registerDriver(driver);
-
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
     public static void main(String[] args) throws SQLException {
-
-        Connection connection;
-        Driver driver = new FabricMySQLDriver();
-        DriverManager.registerDriver(driver);
-
-        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        connectDB();
     }
 }
